da363102a7ebe9ddc459c0dfdb8867bfefaea949
triplea19c <triplea19c@gmail.com>
2022-06-17 13:42:04 +0000

    Assignment done

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = 'Andy';
+let welcomeMessage = "Welcome to Flatbook, Andy!";
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, ANDY!"
+let shortGreeting = "Welcome, A!"
